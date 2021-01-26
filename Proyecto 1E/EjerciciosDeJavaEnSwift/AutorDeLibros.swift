ef091d42d6af8370f6126a374ec6f953dea3b7ea
Ronaldo Andre Vargas Huaman <ronaldovh99@gmail.com>
2021-01-26 13:06:40 -0500

    clase autor agregando Test

---
+++ Proyecto 1E/EjerciciosDeJavaEnSwift/AutorDeLibros.swift
@@ -0,0 +1,31 @@
+//
+//  AutorDeLibros.swift
+//  Proyecto 1E
+//
+//  Created by Ronaldo Andre Vargas Huaman on 1/26/21.
+//
+
+import Foundation
+
+public class Autor{
+    
+    private var nombre: String
+    private var gmail: String
+    private var genero: String
+    
+    init(nombre: String,gmail: String,genero:String) {
+        self.nombre = nombre
+        self.gmail = gmail
+        self.genero = genero
+    }
+    func toString(autor:Autor){
+        print("El nombre es: \(nombre)")
+        print("El gmail es: \(gmail)")
+        print("El genero es: \(genero)")
+        
+    }
+    
+}
+
+
+
---
+++ Proyecto 1ETests/EjerciciosDeJavaEnSwiftTest/AutorDeLibrosTest.swift
@@ -0,0 +1,36 @@
+//
+//  AutorDeLibrosTest.swift
+//  Proyecto 1ETests
+//
+//  Created by Ronaldo Andre Vargas Huaman on 1/26/21.
+//
+
+import XCTest
+@testable import Proyecto_1E
+
+class AutorDeLibrosTest: XCTestCase {
+    var autosDeLibros: Autor!
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        var autosDeLibros =  Autor(nombre: <#T##String#>, gmail: <#T##String#>, genero: <#T##String#>)
+
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
