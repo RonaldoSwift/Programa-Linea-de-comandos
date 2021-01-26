3a8587ed3af219382124f871732ed4cf78525d71
Ronaldo Andre Vargas Huaman <ronaldovh99@gmail.com>
2021-01-26 13:14:51 -0500

    test

--- Proyecto 1ETests/EjerciciosDeJavaEnSwiftTest/AutorDeLibrosTest.swift
+++ Proyecto 1ETests/EjerciciosDeJavaEnSwiftTest/AutorDeLibrosTest.swift
@@ -13,7 +13,7 @@ class AutorDeLibrosTest: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        var autorDeLibros =  Autor(nombre: "Juan", gmail: "juan@gmail", genero: "M")
+        autorDeLibros =  Autor(nombre: "Juan", gmail: "juan@gmail", genero: "M")
 
     }
 
