62ccbbb85554f4b3478a2ef52ea37e2704760822
Ronaldo Andre Vargas Huaman <ronaldovh99@gmail.com>
2021-02-01 13:43:06 -0500

    Agregando funcion de radio al test

--- Proyecto 1ETests/Ejercicio1CirculoTest.swift
+++ Proyecto 1ETests/Ejercicio1CirculoTest.swift
@@ -11,33 +11,41 @@ import XCTest
 class Ejercicio1CirculoTest: XCTestCase {
     var circulo1 : Circulo!
     var circulo2 : Circulo!
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         circulo1 = Circulo(radio: 1.0)
         circulo2 = Circulo(color: "rojo")
     }
-
+    
     override func tearDownWithError() throws {
         
         
     }
-
+    
     func testAreaDeCirculo1() throws {
         let area = circulo1.getArea()
         XCTAssertEqual(3.14, area)
+    }
+    func testRadioDeCirculo1() throws {
+        let radio = circulo1.getRadiu()
+        XCTAssertEqual(3.14, radio)
     }
     
     func testAreaDeCirculo2() throws {
         let area = circulo2.getArea()
         XCTAssertEqual(78.5, area)
     }
-
+    func testRadioDeCirculo2() throws {
+        let radio = circulo2.getRadiu()
+        XCTAssertEqual(5.0, radio)
+    }
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
 }
