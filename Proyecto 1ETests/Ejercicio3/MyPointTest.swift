077e0b63ce0012aff3fd695665f2a982ad11b170
Ronaldo Andre Vargas Huaman <ronaldovh99@gmail.com>
2021-02-02 11:41:30 -0500

    Agregando funciones con los 2 constructores

--- Proyecto 1ETests/Ejercicio3/MyPointTest.swift
+++ Proyecto 1ETests/Ejercicio3/MyPointTest.swift
@@ -10,10 +10,12 @@ import XCTest
 
 class MyPointTest: XCTestCase {
     var myPoint: MyPoint!
+    var myPoint2: MyPoint!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         myPoint = MyPoint.init()
+        myPoint2 = MyPoint.init(x: 0, y: 0)
     }
 
     override func tearDownWithError() throws {
@@ -34,6 +36,19 @@ class MyPointTest: XCTestCase {
     func testDistanceXY() throws {
         let distanciaxy = myPoint.distance(x: 0, y: 0)
         XCTAssertEqual(0, distanciaxy)
+        
+        let punto3: MyPoint = MyPoint()
+        let distanciaEntrePunto1Y3 = myPoint.distance(myPointAnother: punto3)
+        XCTAssertEqual(0, distanciaEntrePunto1Y3)
+        
+    }
+    
+    func testDistanceMyPoint2() throws {
+        let distnciaMyPoint2 = myPoint2.distance(x: 0, y: 0)
+        XCTAssertEqual(0, distnciaMyPoint2)
+        let punto4: MyPoint = MyPoint()
+        let distanciaEntrePunto1Y4 = myPoint2.distance(myPointAnother: punto4)
+        XCTAssertEqual(0, distanciaEntrePunto1Y4)
     }
     
     func testPerformanceExample() throws {
