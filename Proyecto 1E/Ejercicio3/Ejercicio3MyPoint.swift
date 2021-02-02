1785504b8419c298d7ffa7c72e86f3816d8e8b72
Ronaldo Andre Vargas Huaman <ronaldovh99@gmail.com>
2021-02-02 10:48:38 -0500

    Ejercicio 3 MyPoint

---
+++ Proyecto 1E/Ejercicio3/Ejercicio3MyPoint.swift
@@ -0,0 +1,54 @@
+//
+//  Ejercicio3MyPoint.swift
+//  Proyecto 1E
+//
+//  Created by Ronaldo Andre Vargas Huaman on 2/2/21.
+//
+
+import Foundation
+
+class MyPoint {
+    
+    var x: Int
+    var y: Int
+    
+    init() {
+        self.x = 0
+        self.y = 0
+    }
+    
+    init(x:Int, y:Int) {
+        self.x = x
+        self.y = y
+    }
+    
+    func getX() ->Int {
+        return x
+    }
+    
+    func setY(y: Int) {
+        self.y = y
+    }
+    
+    func setXY(x:Int, y:Int) {
+        self.x = x
+        self.y = y
+    }
+    
+    func toString() ->String {
+        let cadena: String = "El numero x es: \(x) y el numero y es: \(y)"
+        return cadena
+    }
+    
+    func distance(x: Int, y:Int) ->Double {
+        let xDiff = self.x - x
+        let yDiff = self.y - y
+        return Double(Int(sqrt(Double(xDiff*xDiff + yDiff*yDiff))))
+    }
+    
+    func distance(MyPointAnother: Double) ->Double {
+        let xDiff = self.x - MyPointAnother.x
+        let yDiff = self.y - MyPointAnother.y
+        return sqrt(Double(xDiff*xDiff + yDiff * yDiff))
+    }
+}
